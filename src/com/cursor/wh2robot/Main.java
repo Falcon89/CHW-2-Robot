e20eea62c4ea2f28a1fbf520064a425ad05b2517
falcon89 <vasyl.kachala@gmail.com>
2019-06-24 10:39:04 +0300

    add Main class and Task

---
+++ src/com/cursor/wh2robot/Main.java
@@ -0,0 +1,22 @@
+package com.cursor.wh2robot;
+
+public class Main {
+    /**
+     * Created by V.Kachala on 24/06/2019.
+     */
+//  Task
+  /*1. Create a robot and its 2 newer versions, let it shoot, shout something, and move. Make him "strong armor"
+    so that nobody could reprogram him and make a mistake in his system so that he cursed all people. Make a controller
+    that will issue a random number from 1 to 12 (see Random ()) and if the batch is from 1-3
+    then the robot will take the form 1, 4-8 form 2 and 9-12 form 3.*/
+//  Addition to the task.
+  /*2. Your work was kidnapped by aliens and it is known that they fly in a certain galaxy which consists of three planets:
+    the planet of the seas, the wind, and the sun. Your robot is not adapted to such conditions and he is there.
+    In order not to happen, you need to redo it so that the corresponding planet will be transformed accordingly.
+    Where to fly non-planets - not known, can on planet A, can B, and can and C. If A will be an implemention of Work Under the sea, V-wind, С-sun.
+    Make also a rand, but the rand drop out have the letters (A-C).*/
+
+    public static void main(String[] args) {
+        System.out.println("CHW2");
+    }
+}
